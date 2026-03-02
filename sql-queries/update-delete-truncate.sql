7219c1c0cd68ef1ca4825c8abf005623f68624bc
Mateus Cunha <mateuscunhazp@gmail.com>
2026-03-02 09:51:01 -0300

    update, delete, truncate

---
+++ sql-queries/update-delete-truncate.sql
@@ -0,0 +1,43 @@
+alter table people /*apaguei uma coluna que não tinha no do Guanabara*/
+drop column codigo;
+
+select * from cursos;
+
+insert into cursos values
+('1', 'HTML4', 'Curso de HTML5', '40', '37', '2014'),
+('2', 'Algoritmos', 'Lógica de Programação', '20', '15', '2014'),
+('3', 'Photoshop', 'Dicas de Photoshop CC', '10', '8', '2014'),
+('4', 'PGP', 'Curso de PHP para inciantes', '40', '20', '2010'),
+('5', 'Jarva', 'Introdução à Linguagem Java', '10', '29', '2000'),
+('6', 'MySQL', 'Bancos de Dados MySQL', '30', '15', '2016'),
+('7', 'Word', 'Curso completo de Word', '40', '30', '2016'),
+('8', 'Sapateado', 'Danças Rítmicas', '40', '30', '2018'),
+('9', 'Cozinha Árabe', 'Aprenda a fazer Kibe', '40', '30', '2018'),
+('10', 'YouTuber', 'Gerar polêmica e ganhar inscritos', '5', '2', '2018');
+
+update cursos
+set `nome` = 'HTML5'
+where idcurso = '1';
+
+update cursos
+set nome = 'PHP', ano = '2015'
+where idcurso = '4';
+
+update cursos
+set nome = 'Java', carga = '40', ano = '2015'
+where idcurso = '5'
+limit 1; /*isso limita que o comando afete somente uma linha*/
+
+update cursos
+set ano = '2050', carga = '800'
+where ano = '2018';
+
+update cursos 
+set ano = '2018', carga = '0'
+where ano = '2050'
+limit 2;
+
+delete from cursos
+where idcurso = '10';
+
+truncate table cursos;
